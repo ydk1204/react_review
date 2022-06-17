4f12b61efa6a8d780c78344fb171f62e64c97eca
ydk1204 <ejrbdi@naver.com>
2022-06-17 23:14:43 +0900

    Route path change

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`/movie/:id`} element={<Detail />} />
+        <Route path={`/`} element={<Home />} />
       </Routes>
     </Router>
   )
