3669c1bb8d8f438910c1813157ced7c717b84f43
ydk1204 <ejrbdi@naver.com>
2022-06-17 23:09:43 +0900

    Router basename add

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from './routes/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
